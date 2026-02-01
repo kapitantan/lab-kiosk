5e1f891f0124d0fc55867c0248ececf999ea374c
kidarikuto <tan04tan20@gmail.com>
2026-02-02 00:29:59 +0900

    chore: urls.pyの整形

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path,include
-from store.api.views import PurchaseView, ProductRegisterView
+from django.urls import include, path
+from store.api.views import (
+    ProductRegisterView,
+    PurchaseView,
+)
 
 urlpatterns = [
-    path('purchases',PurchaseView.as_view(),name='purchases'),
-    path('products/register',ProductRegisterView.as_view(),name='product_register'),
-    
+    path("purchases", PurchaseView.as_view(), name="purchases"),
+    path("products/register", ProductRegisterView.as_view(), name="product_register"),
+
 ]
